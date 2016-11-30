1d32a92f45b83d802e2cf92e20f2682dd99ae7b8
edwinhh <qingfengerya@163.com>
2016-11-30 20:50:33 +0800

    第十三课

--- article/admin.py
+++ article/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from article.models import Article
+from comment.models import Comment
+
+class Commentinline(admin.TabularInline):
+    model = Comment
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("block","title","content","owner","status","create_timestamp","last_update_timestamp")
+    list_display = ("title","content","block","owner","status","create_timestamp","last_update_timestamp")
+    actions = ["make_good"]
+    inlines = [Commentinline]
+
+    # fieldsets = (
+    #     ("基本",{"classes":('collapse',),"fields":("title","content")}),
+    #     ("高级",{"classes":('collapse',),"fields":("status",)})
+    # )
+    #
+    # readonly_fields = ("create_timestamp","last_update_timestamp","title","content","status")
+    def make_good(ModelAdmin,request,queryset):
+        for i in queryset:
+            i.status=10
+            i.save()
+    make_good.short_description = "精华"
+
+
+
+
 
 admin.site.register(Article,ArticleAdmin)
---
+++ article/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ArticleAppconfig(AppConfig):
+
+    name='article'
+    verbose_name = "文章"
